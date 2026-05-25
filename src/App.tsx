edaf5c6ec5144cbe019bfd46b5236035eec15697
Landry Amengne <landryamengne@gmail.com>
2026-05-25 13:59:40 +0200

    Add Vercel Analytics component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import Home from "@/components/pages/HomePages";
 import WhatsAppFloatingButton from "@/components/ui/WhatsAppFloatingButton";
 import AlertModal from "@/components/ui/AlertModal";
 
+// Import du module Analytics
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   const showLock = false; 
 
   return (
     <Layout>
-      <Home /> {/*  portfolio reste affiché */}
+      <Home /> 
       <WhatsAppFloatingButton />
-      <AlertModal show={showLock} /> {/* Modal par-dessus */}
+      <AlertModal show={showLock} /> 
+
+      {/*  Composant Analytics placé  */}
+      <Analytics />
     </Layout>
   );
 }
